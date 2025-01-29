74befb1b2f111a52f1379bdf9fcc5279bdb1c231
maniospas <maniospas@hotmail.com>
2025-01-29 18:39:41 +0200

    added license header to all source

--- src/result.cpp
+++ src/result.cpp
@@ -1,27 +1,27 @@
+/*
+   Copyright 2024 Emmanouil Krasanakis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 #include "Result.h"
-#include "data/Data.h" // Assuming Data is implemented elsewhere
+#include "data/Data.h"
 
-
-Result::Result(DataPtr data) noexcept : data(data)  {
-    data.existsAddOwner();
-}
-
-//Result::Result(const DataPtr& data) noexcept : data(data) {
-//    if (data.exists()) data->addOwner();
-//}
-
-Result::Result(Result& other) noexcept : data(other.data) {
-    //other.data = nullptr;
-    data.existsAddOwner();
-}
-
-Result::Result(Result&& other) noexcept : data(std::move(other.data)) {
-    other.data = DataPtr::NULLP;
-}
-
-Result::~Result() {
-    data.existsRemoveFromOwner();
-}
+Result::Result(DataPtr data) noexcept : data(data)  {data.existsAddOwner();}
+Result::Result(Result& other) noexcept : data(other.data) {data.existsAddOwner();}
+Result::Result(Result&& other) noexcept : data(std::move(other.data)) {other.data = DataPtr::NULLP;}
+Result::~Result() {data.existsRemoveFromOwner();}
+const DataPtr& Result::get() const {return data;}
 
 Result& Result::operator=(const Result& other) {
     if (this != &other) {
@@ -42,4 +42,3 @@ Result& Result::operator=(Result&& other) noexcept {
     return *this;
 }
 
-const DataPtr& Result::get() const {return data;}
